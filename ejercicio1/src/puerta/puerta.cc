932cbc96e325f187986c923688138b3454a8ee02
Diego Nieto Cid <dnietoc@gmail.com>
2015-03-23 21:04:25 -0300

    Se usa ArgParser y Logger/LoggerRegistry en programa 'puerta'

--- ejercicio1/src/puerta/puerta.cc
+++ ejercicio1/src/puerta/puerta.cc
@@ -1,4 +1,19 @@
-int main()
+#include <ArgParser.h>
+#include <logging/Logger.h>
+#include <logging/LoggerRegistry.h>
+
+int main(int argc, char** argv)
 {
-	return 0;
+    ArgParser& args = ArgParser::getInstance ();
+    args.parse (argc, argv);
+
+    LoggerRegistry& registry = LoggerRegistry::getInstance ();
+    registry.application ("puerta");
+    registry.filename ("ejercicio1.log");
+    registry.quiet (!args.debug ());
+
+    Logger& logger = LoggerRegistry::getLogger ("main");
+    logger << "Mensaje de prueba" << Logger::endl;
+
+    return 0;
 }
