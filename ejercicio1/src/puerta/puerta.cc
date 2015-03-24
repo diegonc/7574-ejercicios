4c5c5c5a6788bd86a70cfd3e7c52e4881ae658b1
Diego Nieto Cid <dnietoc@gmail.com>
2015-03-24 15:47:10 -0300

    puerta: Se inicializa el museo y el mutex

--- ejercicio1/src/puerta/puerta.cc
+++ ejercicio1/src/puerta/puerta.cc
@@ -1,6 +1,11 @@
 #include <ArgParser.h>
 #include <logging/Logger.h>
 #include <logging/LoggerRegistry.h>
+#include <museo/constantes.h>
+#include <museo/Museo.h>
+#include <system/IPCName.h>
+#include <system/Semaphore.h>
+#include <system/SharedVariable.h>
 
 int main (int argc, char** argv)
 {
@@ -12,26 +17,12 @@ int main (int argc, char** argv)
 	registry.filename ("ejercicio1.log");
 	registry.quiet (!args.debug ());
 
-	Logger& logger = LoggerRegistry::getLogger ("main");
-	// mensaje con nivel por defecto
-	logger << "Mensaje de prueba" << Logger::endl;
-	// mensaje con nivel de error
-	logger << Level::ERROR
-			<< "Error: mensaje de prueba"
-			<< Logger::endl;
-
-	try {
-		// mensaje con nivel ubicado en lugar inválido
-		logger << "Mensaje" << Level::DEBUG << "Otro mensaje"
-				<< Logger::endl;
-		logger << Level::ERROR
-				<< "El logger debe arrojar excepción"
-				<< Logger::endl;
-	} catch (std::logic_error& e) {
-		logger << Level::INFO
-				<< "El logger funciona correctamente"
-				<< Logger::endl;
-	}
+	SharedVariable<Museo> museo (
+			IPCName(constantes::PATH_NAME, constantes::AREA_MUSEO),
+			0666);
+	Semaphore mutex(
+			IPCName(constantes::PATH_NAME, constantes::SEM_MUTEX),
+			1, 0666);
 
 	return 0;
 }
