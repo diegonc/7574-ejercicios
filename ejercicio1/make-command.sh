3b8f2e118975d837f3fd5a8d7486f9baeb311e91
Diego Nieto Cid <dnietoc@gmail.com>
2015-03-25 19:49:27 -0300

    make-command.sh: se corre bootstrap.sh si configure no existe

--- ejercicio1/make-command.sh
+++ ejercicio1/make-command.sh
@@ -5,6 +5,7 @@ shift
 if [ ! -f Makefile -a -f config.status ]; then
 	./config.status
 elif [ ! -f Makefile ]; then
+	test -f configure || ./bootstrap.sh
 	./configure \
 		CFLAGS="-O0 -g3 -gdwarf-2" \
 		CXXFLAGS="-O0 -g3 -gdwarf-2" \
